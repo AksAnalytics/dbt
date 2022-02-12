0f60c420fcd4c7e351ed5469b28fc0f95481ad1c
williamdst <schdwilliams@gmail.com>
2022-02-12 00:57:52 -0500

    Added the new procedure-dumper.py to the repo

--- utils/Data-Mart-Dump/procedure-dumper.py
+++ utils/Data-Mart-Dump/procedure-dumper.py
@@ -22,23 +22,48 @@ TXT_FILE_LOCATION = 'dw_procedures.txt'
 # Change this to where you want the files to be dumped, otherise they will be dumped in the current working directory
 # WRITE_LOCATION = r'/Users/williamdst/Documents/EON-Files/SBD-DBT-DEMO/dbt-data-marting-framework/umm/models/onboarding/stage'
 
-def main() -> None:
-    """ Generates a collection of crt_<table_name>.sql files that can be executed in the dbt project"""
+def write_sources_file(ddl_statements: str):
+    """Writes the dbt sources.yml file from a Redshift dump
 
-    # Read the contents of the file.
-    with open(TXT_FILE_LOCATION, "r") as f:
-        ddl_statements = f.read()
-        assert ddl_statements, "File has no contents"
+    :param ddl_statements: A collection of ddl_statments read from a text file.
+    :return: Writes to a sources.yml file.
+    """
 
+    # Find all the source tables
     sources_yml = sorted(set(re.findall(r'\w*\..*\_current', ddl_statements)))
     sources_yml_no_current = [source.replace('_current', '') for source in sources_yml]
     schema_names = sorted(set([table_name.split('.')[0] for table_name in sources_yml_no_current if re.match(r"\w*\.\w*", table_name)]))
-    print(schema_names)
+
+    database_line = "database: RAW"
+    source_yml_file = "version: 2\n\nsources:"
+
+    for schema in schema_names:
+        schema_header = f"\n  - name: {schema}\n    {database_line}\n    tables:\n"
+        source_yml_file += schema_header
+
+        for source in sources_yml_no_current:
+            if schema in source:
+                source_header = f"      - name: {source.replace(f'{schema}.', '')}\n"
+                source_yml_file  += source_header
+
+
+    os.chdir(WRITE_LOCATION)
+    file = open(f'sources.yml', 'w')
+    file.write(source_yml_file)
+    file.close()
+    return None
+
+
+def separate_procedures(ddl_statements: str):
+    """Breaks each procedure into a separate file of its own.
+
+    :param ddl_statements: A collection of ddl_statments read from a text file.
+    :return: Writes to a series of proc_ files
+    """
 
     # BREAK THE PROCEDURES INTO DIFFEERENT FILES
     pattern = r"\n\$\$\n;"
     start_extract = 0
-    i = 0
     for match in re.finditer(pattern, ddl_statements):
         procedure_definition = ddl_statements[start_extract: match.end()]
         procedure_name = re.findall(r'CREATE OR REPLACE PROCEDURE (\w*\.\w*)', procedure_definition)[0]
@@ -60,16 +85,23 @@ def main() -> None:
             procedure_definition = procedure_definition.replace(raw, no_source)
         """
 
-        """
         os.chdir(WRITE_LOCATION)
         file = open(f'proc_{procedure_name.strip()}.sql', 'w')
         file.write(procedure_definition)
         file.close()
-        """
 
-        start_extract = match.end()+1
+        start_extract = match.end() + 1
 
     return
 
+
+
 if __name__ == '__main__':
-    main()
+    """Generates a collection of crt_<table_name>.sql files that can be executed in the dbt project"""
+    # Read the contents of the file.
+    with open(TXT_FILE_LOCATION, "r") as f:
+        ddl_statements = f.read()
+        assert ddl_statements, "File has no contents"
+
+    write_sources_file(ddl_statements)
+    separate_procedures(ddl_statements)
